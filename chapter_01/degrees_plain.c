1c6b7053bb80cdd5ee2dca6ac7b4e28274e268c2
Raghavendra Ugare <raghu.ugare@gmail.com>
2014-12-05 18:28:23 +0530

    degrees conversion program (initial version)

---
+++ chapter_01/degrees_plain.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int celsius, fahr, step, lower, upper;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+	
+    fahr = lower;
+    while(fahr<=upper) {
+	   celsius = 5*(fahr - 32)/9;
+	   printf("%d\t%d\n", fahr, celsius);
+	   fahr = fahr + step;
+    } // end while
+
+    return 0;
+} // end main()
